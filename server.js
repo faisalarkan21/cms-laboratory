2443688615644d1733a2a321a48f1f2fcc3312e5
faisalarkan21 <faisalarkan21@gmail.com>
2017-02-12 18:00:22 +0700

    add  concurrently -> scipts "start:server" and "start:client"

---
+++ server.js
@@ -0,0 +1,14 @@
+
+
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.set('port', (process.env.PORT || 3001));
+
+app.listen(app.get('port'),  () => {
+  console.log(`Server berjalan di port : http://localhost:${app.get('port')}/`)
+})
