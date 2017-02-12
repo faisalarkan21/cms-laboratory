ce7781c1b543ace91f7c7018d2259ff9065d55ac
faisalarkan21 <faisalarkan21@gmail.com>
2017-02-12 18:04:31 +0700

    add concurrently -> scripts "start:server" and "start:client"

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Haii !')
 })
 
 app.set('port', (process.env.PORT || 3001));
--- start-client.js
+++ start-client.js
@@ -1,3 +1,6 @@
+
+// Buat Akses ke Shell pakai child_process
+
 const args = [ 'start' ];
 const opts = { stdio: 'inherit', cwd: 'client', shell: true };
 require('child_process').spawn('npm', args, opts);
