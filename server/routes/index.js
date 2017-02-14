cd6f2df89666694d13d8f46ea9603c530b3b1463
faisalarkan21 <faisalarkan21@gmail.com>
2017-02-14 22:42:11 +0700

    NeDB tested success

---
+++ server/routes/index.js
@@ -0,0 +1,35 @@
+var Datastore = require('nedb'),
+    db = new Datastore({
+        filename: './database/database-cms',
+        autoload: true
+    });
+
+
+var people = [];
+
+var scott = {  
+    name: 'Scott Robinson',
+    age: 28,
+    twitter: '@ScottWRobinson'
+};
+
+var elon = {  
+    name: 'Elon Musk',
+    age: 44,
+    twitter: '@elonmusk'
+};
+
+var jack = {  
+    name: 'Jack Dorsey',
+    age: 39,
+    twitter: '@jack'
+};
+
+people.push(scott, elon, jack);
+
+db.insert(people, function(err, docs) {  
+    docs.forEach(function(d) {
+        console.log('Saved user:', d.name);
+    });
+});
+
--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@
 
 var express = require('express');
 var app = express()
+var routes = require('./routes/index');
+
 
 app.get('/pesan', function (req, res) {
   res.json({pesan: 'selamat datang XD '});
