d532788a5a77edf88327c08354bd1c3f93c10fe3
faisalarkan21 <faisalarkan21@gmail.com>
2017-02-16 10:41:26 +0700

    grid-flexbox cdn

--- client/src/components/hal-awal/Welcome.js
+++ client/src/components/hal-awal/Welcome.js
@@ -9,36 +9,39 @@ import './welcome.css'
 // const {Grid, Row, Col} = require('react-flexbox-grid');
 
 const styles = {
-  div:{
-    display: 'flex',
-    flexDirection: 'row wrap',
-    padding: 20,
-    width: '100%'
-  },
-  paperLeft:{
-    flex: 1,
-    height: '100%',
-    margin: 10,
-    textAlign: 'center',
-    padding: 10
-  },
-  paperRight:{
-    height: 600,
-    flex: 3,
-    margin: 10,
-    textAlign: 'center',
-  }
+    div: {
+        // display: 'flex',
+        // flexDirection: 'row warp',
+        // padding: 20,
+        // justifyContent: 'center',
+      
+        // maxWidth: '70%'
+    },
+    paperLeft: {
+        flex: 4,
+        height: '100%',
+        margin: 10,
+        textAlign: 'center',
+        padding: 10
+    },
+    paperRight: {
+        height: 400,
+        // flex: 1,
+        // width:'20%',
+        // order:4,  flexShrink: 4,
+        
+        // margin: 10,
+        textAlign: 'center'
+    }
 };
 class Welcome extends Component {
     render() {
         return (
             <MuiThemeProvider>
-                <div>
-                    <div style={styles.div}>
-                        <Paper zDepth={3} style={styles.paperLeft}>
-                            <h4>First Vertical component</h4>
-                        </Paper>
-                        <Paper zDepth={3} style={styles.paperRight}>
+                <div className="row">
+                    <div className="col-md-12">
+                      
+                        <Paper zDepth={3} style={styles.paperRight} className="col-xs-offset-2 col-xs-9">
                             <h4>Second Vertical component</h4>
                         </Paper>
                     </div>
